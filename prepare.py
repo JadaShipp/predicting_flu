97f7430f5acc3f1c646aeaa29523876f5436c432
jadashipp <jada.l.shipp@gmail.com>
2020-07-15 20:30:53 -0500

    added loop for percent nans

--- prepare.py
+++ prepare.py
@@ -6,15 +6,11 @@ from sklearn.preprocessing import OneHotEncoder
 
 
 def label_encode_columns(df):
-    train, test = train_test_split(df, random_state=123, train_size=.80)
 
     encoder = LabelEncoder()
 
-    encode_list = [
-    'gender', 'partner', 'dependents', 'phone_service'
-    , 'multiple_lines', 'online_security', 'online_backup'
-    , 'device_protection', 'tech_support'
-    , 'streaming_movies', 'streaming_tv', 'paperless_billing', 'churn'
+    encode_list = [''
+
     ]
              
     for e in encode_list:
@@ -27,3 +23,18 @@ def one_hot_encode_columns(df):
     encode_list = [
         ''
     ]
+
+def percent_nans(df):
+    '''
+    Takes in a dataframe 
+    '''
+    x = ['column_name','n_nans', 'percentage_nans']
+    missing_data = pd.DataFrame(columns=x)
+    columns = df.columns
+    for col in columns:
+        acolumn_name = col
+        amissing_data = df[col].isnull().sum()
+        amissing_in_percentage = (df[col].isnull().sum()/df[col].shape[0])*100
+        
+        missing_data.loc[len(missing_data)] = [acolumn_name, amissing_data, amissing_in_percentage]
+    return missing_data
