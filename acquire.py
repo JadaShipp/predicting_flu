65746989f611e18b84da093c6c3993bab89c184c
jadashipp <jada.l.shipp@gmail.com>
2020-07-18 15:00:25 -0500

    added doc strings to acquire functions

--- acquire.py
+++ acquire.py
@@ -5,7 +5,8 @@ from sklearn.model_selection import train_test_split
 
 def acquire_training_data():
     '''
-    
+    Reads in feature csv data from folder and converts it to pandas dataframe
+    Returns dataframe
     '''
     # Read in the training set feature csv using pandas
     df = pd.read_csv('Flu_Shot_Learning_Predict_H1N1_and_Seasonal_Flu_Vaccines_-_Training_Features.csv', 
@@ -14,6 +15,10 @@ def acquire_training_data():
     return df
 
 def acquire_target_variable_data():
+    '''
+    Reads in target variable csv data from folder and converts it to pandas dataframe
+    Returns dataframe
+    '''
     # Read in the training set target variable csv using pandas
     df = pd.read_csv('Flu_Shot_Learning_Predict_H1N1_and_Seasonal_Flu_Vaccines_-_Training_Labels.csv', 
     index_col=0)
@@ -21,6 +26,11 @@ def acquire_target_variable_data():
     return df
 
 def acquire_data():
+    '''
+    Merges feature dataframe and target variable dataframe,
+    Drops duplicate respondent_id column
+    Returns dataframe
+    '''
     feature_df = acquire_training_data()
     target_variable_df = acquire_target_variable_data()
 
