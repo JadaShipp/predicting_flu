3d4c2364cbbcb34e9239ba4330bf30a8c4624386
jadashipp <jada.l.shipp@gmail.com>
2020-07-18 17:27:41 -0500

    fixed error in imports

--- prepare.py
+++ prepare.py
@@ -1,9 +1,12 @@
-import pandas as import pd
+import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 
-
+# ---------------------#
+#  Initial Evaluation  #
+#      Functions       #
+# ---------------------#
 
 def percent_nans(df):
     '''
@@ -21,12 +24,20 @@ def percent_nans(df):
         missing_data.loc[len(missing_data)] = [acolumn_name, amissing_data, amissing_in_percentage]
     return missing_data
 
+# ---------------------#
+#  Prepare functions   #
+# ---------------------#
+
 
 def fill_null_values(train, test):
     train = train.apply(lambda x:x.fillna(x.value_counts().index[0]))
     test = test.apply(lambda x:x.fillna(x.value_counts().index[0]))
 
     return train, test
+
+# ---------------------#
+#    Label Encoding    #
+# ---------------------#
 
 def label_encode_columns(train, test):
 
@@ -44,7 +55,9 @@ def label_encode_columns(train, test):
     
     return train, test
 
-
+# ---------------------#
+#   One Hot Encoding   #
+# ---------------------#
 
 def ohe_age_group(train, test):
     '''
@@ -78,4 +91,102 @@ def ohe_age_group(train, test):
 
 
 
+def ohe_education(train, test):
+    # Encode education column
 
+    # Create encoder object
+    encoder = OneHotEncoder()
+
+    # Fit on the age_group column of the train df
+    encoder.fit(train[['education']])
+
+    # nice columns for display
+    cols = ['education_' + c for c in encoder.categories_[0]]
+
+    # Transform the column on train and test and concatenate new df onto train and test dfs
+    m = encoder.transform(train[['education']]).todense()
+    train = pd.concat([
+        train,
+        pd.DataFrame(m, columns=cols, index=train.index)
+    ], axis=1)
+
+    m = encoder.transform(test[['education']]).todense()
+    test = pd.concat([
+        test,
+        pd.DataFrame(m, columns=cols, index=test.index)
+    ], axis=1)
+
+    return train, test
+
+def ohe_race(train, test):
+    # Encode race column
+
+    # Create encoder object
+    encoder = OneHotEncoder()
+
+    # Fit on the age_group column of the train df
+    encoder.fit(train[['race']])
+
+    # nice columns for display
+    cols = ['race_' + c for c in encoder.categories_[0]]
+
+    # Transform the column on train and test and concatenate new df onto train and test dfs
+    m = encoder.transform(train[['race']]).todense()
+    train = pd.concat([
+        train,
+        pd.DataFrame(m, columns=cols, index=train.index)
+    ], axis=1)
+
+    m = encoder.transform(test[['race']]).todense()
+    test = pd.concat([
+        test,
+        pd.DataFrame(m, columns=cols, index=test.index)
+    ], axis=1)
+
+    return train, test
+
+def ohe_income_poverty(train,test):
+    # Encode income_poverty column
+
+    # Create encoder object
+    encoder = OneHotEncoder()
+
+    # Fit on the age_group column of the train df
+    encoder.fit(train[['income_poverty']])
+
+    # nice columns for display
+    cols = ['income_poverty_' + c for c in encoder.categories_[0]]
+
+    # Transform the column on train and test and concatenate new df onto train and test dfs
+    m = encoder.transform(train[['income_poverty']]).todense()
+    train = pd.concat([
+        train,
+        pd.DataFrame(m, columns=cols, index=train.index)
+    ], axis=1)
+
+    m = encoder.transform(test[['income_poverty']]).todense()
+    test = pd.concat([
+        test,
+        pd.DataFrame(m, columns=cols, index=test.index)
+    ], axis=1)
+
+    return train, test
+
+# ---------------------#
+#        Scaling       #
+# ---------------------#
+
+def scale_minmax(train, test, column_list):
+    scaler = MinMaxScaler()
+    column_list_scaled = [col + '_scaled' for col in column_list]
+    train_scaled = pd.DataFrame(scaler.fit_transform(train[column_list]), 
+                                columns = column_list_scaled, 
+                                index = train.index)
+    train = train.join(train_scaled)
+
+    test_scaled = pd.DataFrame(scaler.transform(test[column_list]), 
+                                columns = column_list_scaled, 
+                                index = test.index)
+    test = test.join(test_scaled)
+
+    return train, test
