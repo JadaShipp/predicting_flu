062d3e1a858e5ab812bfa4f0d14020ccce4353cb
jadashipp <jada.l.shipp@gmail.com>
2020-07-19 21:32:04 -0500

    start explore script

---
+++ explore.py
@@ -0,0 +1,20 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import pandas as pd
+import numpy as np 
+import acquire
+import prepare
+
+#Data Visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#Set figure size and figure size for all plots
+plt.rc("figure", figsize = (14,14))
+plt.rc("font", size=14)
+
+# Allow all columns to be displayed
+pd.set_option('display.max_columns', None)
+
+
