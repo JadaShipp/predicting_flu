1717ba2c1e005603cc889903a354608129f23bc3
jadashipp <jada.l.shipp@gmail.com>
2020-09-02 16:33:11 -0500

    add boolean columns to x_sets

--- modeling.py
+++ modeling.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np 
 import acquire
 import prepare
+import explore
 
 # Modeling
 
@@ -22,75 +23,51 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 def create_h1n1_validate_set(train, test):
+    train = explore.feature_engineering(train)
+    test = explore.feature_engineering(test)
+
     train, validate = train_test_split(train, random_state=123, test_size=.2)
     X_train = train[['encoded_sex', 'encoded_marital_status', 'encoded_rent_or_own', 
-                          'encoded_employment_status', #'race_white','education_12_years', 'education_<_12_years',
-                          'education_college_graduate', 'education_some_college', 'age_group_18_-_34_years', 'age_group_35_-_44_years',
-                          'age_group_45_-_54_years', 'age_group_55_-_64_years', 'age_group_65+_years', 
-                          'income_poverty_<=_$75,000,_above_poverty','income_poverty_>_$75,000','income_poverty_below_poverty',
-                          'hhs_geo_region_atmpeygn', 'hhs_geo_region_bhuqouqj','hhs_geo_region_dqpwygqj', 'hhs_geo_region_fpwskwrf',
-                          'hhs_geo_region_kbazzjca', 'hhs_geo_region_lrircsnp', 'hhs_geo_region_lzgpxyit', 'hhs_geo_region_mlyzmhmf', 
-                          'hhs_geo_region_oxchjgsf', 'hhs_geo_region_qufhixun', 'opinion_h1n1_risk_scaled', 'doctor_recc_h1n1','behavioral_antiviral_meds']]
+                          'encoded_employment_status', 'graduated_college', 'age_55_and_up', 'over_75k', 'relevant_geography',
+                          'opinion_h1n1_risk_scaled', 'doctor_recc_h1n1','behavioral_antiviral_meds']]
     y_train = train[['h1n1_vaccine']]
 
 
     X_val = validate[['encoded_sex', 'encoded_marital_status', 'encoded_rent_or_own', 
-                          'encoded_employment_status',# 'race_white','education_12_years', 'education_<_12_years',
-                          'education_college_graduate', 'education_some_college', 'age_group_18_-_34_years', 'age_group_35_-_44_years',
-                          'age_group_45_-_54_years', 'age_group_55_-_64_years', 'age_group_65+_years', 
-                          'income_poverty_<=_$75,000,_above_poverty','income_poverty_>_$75,000','income_poverty_below_poverty',
-                          'hhs_geo_region_atmpeygn', 'hhs_geo_region_bhuqouqj','hhs_geo_region_dqpwygqj', 'hhs_geo_region_fpwskwrf',
-                          'hhs_geo_region_kbazzjca', 'hhs_geo_region_lrircsnp', 'hhs_geo_region_lzgpxyit', 'hhs_geo_region_mlyzmhmf', 
-                          'hhs_geo_region_oxchjgsf', 'hhs_geo_region_qufhixun', 'opinion_h1n1_risk_scaled', 'doctor_recc_h1n1','behavioral_antiviral_meds']]
+                          'encoded_employment_status', 'graduated_college', 'age_55_and_up', 'over_75k', 'relevant_geography',
+                          'opinion_h1n1_risk_scaled', 'doctor_recc_h1n1','behavioral_antiviral_meds']]
     y_val = validate[['h1n1_vaccine']]
 
     X_test = test[['encoded_sex', 'encoded_marital_status', 'encoded_rent_or_own', 
-                          'encoded_employment_status', # 'race_white','education_12_years', 'education_<_12_years',
-                          'education_college_graduate', 'education_some_college', 'age_group_18_-_34_years', 'age_group_35_-_44_years',
-                          'age_group_45_-_54_years', 'age_group_55_-_64_years', 'age_group_65+_years', 
-                          'income_poverty_<=_$75,000,_above_poverty','income_poverty_>_$75,000','income_poverty_below_poverty',
-                          'hhs_geo_region_atmpeygn', 'hhs_geo_region_bhuqouqj','hhs_geo_region_dqpwygqj', 'hhs_geo_region_fpwskwrf',
-                          'hhs_geo_region_kbazzjca', 'hhs_geo_region_lrircsnp', 'hhs_geo_region_lzgpxyit', 'hhs_geo_region_mlyzmhmf', 
-                          'hhs_geo_region_oxchjgsf', 'hhs_geo_region_qufhixun', 'opinion_h1n1_risk_scaled', 'doctor_recc_h1n1','behavioral_antiviral_meds']]
+                          'encoded_employment_status', 'graduated_college', 'age_55_and_up', 'over_75k', 'relevant_geography',
+                          'opinion_h1n1_risk_scaled', 'doctor_recc_h1n1','behavioral_antiviral_meds']]
     y_test = test[['h1n1_vaccine']]   
 
 
     return X_train, y_train, X_val, y_val, X_test, y_test
 
 def create_seas_validate_set(train, test):
+    train = explore.feature_engineering(train)
+    test = explore.feature_engineering(test)
+    
     train, validate = train_test_split(train, random_state=123, test_size=.2)
     X_train = train[['encoded_sex', 'encoded_marital_status', 'encoded_rent_or_own', 
-                          'encoded_employment_status', 'race_black', 'race_hispanic', 
-                          'race_other_or_multiple', 'race_white','education_12_years', 'education_<_12_years',
-                          'education_college_graduate', 'education_some_college', 'age_group_18_-_34_years', 'age_group_35_-_44_years',
-                          'age_group_45_-_54_years', 'age_group_55_-_64_years', 'age_group_65+_years', 
-                          'income_poverty_<=_$75,000,_above_poverty','income_poverty_>_$75,000','income_poverty_below_poverty',
-                          'hhs_geo_region_atmpeygn', 'hhs_geo_region_bhuqouqj','hhs_geo_region_dqpwygqj', 'hhs_geo_region_fpwskwrf',
-                          'hhs_geo_region_kbazzjca', 'hhs_geo_region_lrircsnp', 'hhs_geo_region_lzgpxyit', 'hhs_geo_region_mlyzmhmf', 
-                          'hhs_geo_region_oxchjgsf', 'hhs_geo_region_qufhixun', 'doctor_recc_h1n1','behavioral_antiviral_meds']]
+                          'encoded_employment_status', 'graduated_college', 'age_55_and_up', 'over_75k', 'relevant_geography',
+                          'opinion_h1n1_risk_scaled', 'doctor_recc_h1n1','behavioral_antiviral_meds', 'doctor_recc_seasonal',
+                          'opinion_seas_vacc_effective', 'opinion_seas_risk','opinion_seas_sick_from_vacc']]
     y_train = train[['seasonal_vaccine']]
 
 
     X_val = validate[['encoded_sex', 'encoded_marital_status', 'encoded_rent_or_own', 
-                          'encoded_employment_status', 'race_black', 'race_hispanic', 
-                          'race_other_or_multiple', 'race_white','education_12_years', 'education_<_12_years',
-                          'education_college_graduate', 'education_some_college', 'age_group_18_-_34_years', 'age_group_35_-_44_years',
-                          'age_group_45_-_54_years', 'age_group_55_-_64_years', 'age_group_65+_years', 
-                          'income_poverty_<=_$75,000,_above_poverty','income_poverty_>_$75,000','income_poverty_below_poverty',
-                          'hhs_geo_region_atmpeygn', 'hhs_geo_region_bhuqouqj','hhs_geo_region_dqpwygqj', 'hhs_geo_region_fpwskwrf',
-                          'hhs_geo_region_kbazzjca', 'hhs_geo_region_lrircsnp', 'hhs_geo_region_lzgpxyit', 'hhs_geo_region_mlyzmhmf', 
-                          'hhs_geo_region_oxchjgsf', 'hhs_geo_region_qufhixun', 'doctor_recc_h1n1','behavioral_antiviral_meds']]
+                          'encoded_employment_status', 'graduated_college', 'age_55_and_up', 'over_75k', 'relevant_geography',
+                          'opinion_h1n1_risk_scaled', 'doctor_recc_h1n1','behavioral_antiviral_meds', 'doctor_recc_seasonal',
+                          'opinion_seas_vacc_effective', 'opinion_seas_risk','opinion_seas_sick_from_vacc']]
     y_val = validate[['seasonal_vaccine']]
 
     X_test = test[['encoded_sex', 'encoded_marital_status', 'encoded_rent_or_own', 
-                          'encoded_employment_status', 'race_black', 'race_hispanic', 
-                          'race_other_or_multiple', 'race_white','education_12_years', 'education_<_12_years',
-                          'education_college_graduate', 'education_some_college', 'age_group_18_-_34_years', 'age_group_35_-_44_years',
-                          'age_group_45_-_54_years', 'age_group_55_-_64_years', 'age_group_65+_years', 
-                          'income_poverty_<=_$75,000,_above_poverty','income_poverty_>_$75,000','income_poverty_below_poverty',
-                          'hhs_geo_region_atmpeygn', 'hhs_geo_region_bhuqouqj','hhs_geo_region_dqpwygqj', 'hhs_geo_region_fpwskwrf',
-                          'hhs_geo_region_kbazzjca', 'hhs_geo_region_lrircsnp', 'hhs_geo_region_lzgpxyit', 'hhs_geo_region_mlyzmhmf', 
-                          'hhs_geo_region_oxchjgsf', 'hhs_geo_region_qufhixun', 'doctor_recc_h1n1','behavioral_antiviral_meds']]
+                          'encoded_employment_status', 'graduated_college', 'age_55_and_up', 'over_75k', 'relevant_geography',
+                          'opinion_h1n1_risk_scaled', 'doctor_recc_h1n1','behavioral_antiviral_meds', 'doctor_recc_seasonal',
+                          'opinion_seas_vacc_effective', 'opinion_seas_risk','opinion_seas_sick_from_vacc']]
     y_test = test[['seasonal_vaccine']]   
 
 
