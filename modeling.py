f7a5a47a09cac5538aee0f87d0008c2337a1f3fe
jadashipp <jada.l.shipp@gmail.com>
2020-09-02 17:55:47 -0500

    added functions for final model and prediction df

--- modeling.py
+++ modeling.py
@@ -49,7 +49,7 @@ def create_h1n1_validate_set(train, test):
 def create_seas_validate_set(train, test):
     train = explore.feature_engineering(train)
     test = explore.feature_engineering(test)
-    
+
     train, validate = train_test_split(train, random_state=123, test_size=.2)
     X_train = train[['encoded_sex', 'encoded_marital_status', 'encoded_rent_or_own', 
                           'encoded_employment_status', 'graduated_college', 'age_55_and_up', 'over_75k', 'relevant_geography',
@@ -91,6 +91,71 @@ def logit_regression_model(x_train, y_train):
     conf.index.name = "Actual"
     
     return conf
+
+def create_knn_model(X_test_h1n1, y_test_h1n1, X_test_seas, y_test_seas):
+    # fitting our data to our KNearest Neighbors model
+    knn_t = KNeighborsClassifier(n_neighbors=4)
+    knn_t.fit(X_test_h1n1, y_test_h1n1)
+
+    # creating our confusion matrix
+    y_pred_test = knn_t.predict(X_test_h1n1)
+    pd.DataFrame(confusion_matrix(y_test_h1n1, y_pred_test))
+    #code to label matrix
+    #code to label matrix
+    labels = ['No Vaccine', 'Vaccine']
+    predicted_labels = [name + " Predicted" for name in labels ]
+
+    h1n1_test_confusion_matrix = pd.DataFrame(confusion_matrix(y_pred_test, y_test_h1n1), index=labels, columns=[predicted_labels])
+    h1n1_test_confusion_matrix.index.name = "Actual"
+    h1n1_test_confusion_matrix
+
+    # Print a classification report
+    target_names = ["No Vaccine", "Vaccine"]
+    print(classification_report(y_test_h1n1, y_pred_test, target_names = target_names))
+
+
+    # fitting our data to our KNearest Neighbors model
+    knn_st = KNeighborsClassifier(n_neighbors=4)
+    knn_st.fit(X_test_seas, y_test_seas)
+
+    # creating our confusion matrix
+    y_pred_st = knn_st.predict(X_test_seas)
+    pd.DataFrame(confusion_matrix(y_test_seas, y_pred_st))
+    #code to label matrix
+    #code to label matrix
+    labels = ['No Vaccine', 'Vaccine']
+    predicted_labels = [name + " Predicted" for name in labels ]
+
+    seasonal_test_confusion_matrix = pd.DataFrame(confusion_matrix(y_pred_st, y_test_seas), index=labels, columns=[predicted_labels])
+    seasonal_test_confusion_matrix.index.name = "Actual"
+    seasonal_test_confusion_matrix
+
+    # Print a classification report
+    target_names = ["No Vaccine", "Vaccine"]
+    print(classification_report(y_test_seas, y_pred_st, target_names = target_names))
+
+    return h1n1_test_confusion_matrix, seasonal_test_confusion_matrix
+
+def create_prediction_df(X_test_h1n1, y_test_h1n1, X_test_seas, y_test_seas):
+    knn_t = KNeighborsClassifier(n_neighbors=4)
+    knn_t.fit(X_test_h1n1, y_test_h1n1)
+
+    knn_st = KNeighborsClassifier(n_neighbors=4)
+    knn_st.fit(X_test_seas, y_test_seas)
+
+    # Make predictions and prediction probabilities
+    y_pred_proba_df = knn_t.predict_proba(X_test_h1n1)
+    y_pred_proba_df_s = knn_st.predict_proba(X_test_seas)
+
+    predictions_df = pd.DataFrame(
+    {'Respondent_id': X_test_h1n1.index,
+    'Probability_of_getting_h1n1_vaccine': y_pred_proba_df[:,1],
+    'Probability_of_getting_seasonal_vaccine': y_pred_proba_df_s[:,1]})
+
+    predictions_df = predictions_df.set_index('Respondent_id')
+    return predictions_df
+
+    
 
 
 
