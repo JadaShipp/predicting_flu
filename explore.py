192846e6183ef6c82d809597093c1ffdc9831266
jadashipp <jada.l.shipp@gmail.com>
2020-09-03 13:51:19 -0500

    added all the final functions for exploring

--- explore.py
+++ explore.py
@@ -18,6 +18,10 @@ plt.rc("font", size=14)
 # Allow all columns to be displayed
 pd.set_option('display.max_columns', None)
 
+# ---------------------#
+#     H1N1 Explore     #
+# ---------------------#
+
 
 
 def distribution_of_h1n1_vaccine_status(h1n1_train):
@@ -103,7 +107,7 @@ def opinion_h1n1_status(h1n1_train):
 
 
 
-def create_continueous_heatmap(h1n1_train):
+def create_continuous_heatmap_h1n1(h1n1_train):
     # Subset data to only have opinion
     continuous_h1n1 = h1n1_train[['h1n1_concern',
                                 'h1n1_knowledge',
@@ -176,3 +180,77 @@ def distribution_of_seasonal_vaccine_status(seasonal_train):
     plt.figure(figsize=(15,15))
     seasonal_train.seasonal_vaccine.value_counts().sort_index().plot(kind = "bar",alpha = .5)
     plt.title("Distirbution of Patients' Vaccine Status")
+
+
+
+def opinion_seasonal_status(train):
+    #Create a function that takes in the h1n1 training set and creates a heatmaps
+    plt.rc('figure', figsize=(15, 15))
+    plt.rc('font', size=13)
+    plt.title('Opinion of Seasoonal Risk and Vaccine Status')
+    ctab = pd.crosstab(train.seasonal_vaccine, train.opinion_seas_risk, normalize=True)
+    sns.heatmap(ctab, annot=True, cmap='Purples', fmt='.1%')
+
+def sex_race_education_seasonal(seasonal_train):
+    features = ['sex', 'race', 'education']
+
+    _, ax = plt.subplots(nrows=1, ncols=3, figsize=(25,25))
+
+    vaccination_rate = seasonal_train.seasonal_vaccine.mean()
+
+    for i, feature in enumerate(features):
+        sns.barplot(feature, 'seasonal_vaccine', data=seasonal_train, ax=ax[i], alpha=.9)
+        ax[i].set_ylabel('Vaccination Rate')
+        ax[i].axhline(vaccination_rate, ls='--', color='grey')
+  
+
+def age_income_marital_status_seaonal(seasonal_train):
+    features = ['age_group', 'income_poverty', 'marital_status']
+
+    _, ax = plt.subplots(nrows=1, ncols=3, figsize=(25,25))
+
+    vaccination_rate = seasonal_train.seasonal_vaccine.mean()
+
+    for i, feature in enumerate(features):
+        sns.barplot(feature, 'seasonal_vaccine', data=seasonal_train, ax=ax[i], alpha=.9)
+        ax[i].set_ylabel('Vaccination Rate')
+        ax[i].axhline(vaccination_rate, ls='--', color='grey')
+
+def rent_employment_geo_seaonal(seasonal_train):
+    features = ['rent_or_own', 'employment_status', 'hhs_geo_region']
+
+    _, ax = plt.subplots(nrows=1, ncols=3, figsize=(25,25))
+
+    vaccination_rate = seasonal_train.seasonal_vaccine.mean()
+
+    for i, feature in enumerate(features):
+        sns.barplot(feature, 'seasonal_vaccine', data=seasonal_train, ax=ax[i], alpha=.9)
+        ax[i].set_ylabel('Vaccination Rate', fontsize = 16.0)
+        ax[i].axhline(vaccination_rate, ls='--', color='grey')
+
+
+def create_continuous_heatmap_seasonal(seasonal_train):
+    # Subset data to only have opinion
+    continuous_seasonal = seasonal_train[[
+                                'behavioral_antiviral_meds',
+                                'behavioral_avoidance',
+                                'behavioral_face_mask',
+                                'behavioral_wash_hands',
+                                'behavioral_large_gatherings',
+                                'behavioral_outside_home',
+                                'behavioral_touch_face',
+                                'doctor_recc_seasonal',
+                                'chronic_med_condition',
+                                'child_under_6_months',
+                                'health_worker',
+                                'health_insurance',
+                                'opinion_seas_vacc_effective',
+                                'opinion_seas_risk',
+                                'opinion_seas_sick_from_vacc',
+                                'seasonal_vaccine']]
+    plt.matshow(continuous_seasonal.corr())
+    plt.xticks(range(len(continuous_seasonal.columns)), continuous_seasonal.columns)
+    plt.yticks(range(len(continuous_seasonal.columns)), continuous_seasonal.columns)
+    plt.colorbar()
+    plt.figure(figsize = (10.0, 10.0))
+    plt.show()
